6fb416b33604cf1b7bef7bc8e0d93440f5e679da
KajtiChr <kajchrzanowski@gmail.com>
2020-04-08 13:18:26 +0200

    App change

--- mydockerfrontend/src/App.js
+++ mydockerfrontend/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-          Hello Kajetan !!!
+          Hello Mr. Chrzanowski !!!
         </p>
       </div>
     );
